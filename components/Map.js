46908b50e22568330563bd9f0cf1126d88f27a6f
Programming-School-Pro-Coding <mohamedehabam99@hotmail.com>
2025-02-02 22:46:34 +0200

    .

--- components/Map.js
+++ components/Map.js
@@ -12,7 +12,7 @@ function Map() {
 
         <div className="rounded-lg overflow-hidden shadow-lg hover:shadow-xl transition-shadow duration-300">
           <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3710.755942448073!2d39.19032531541629!3d21.4858066857217!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMjHCsDI5JzA4LjkiTiAzOcKwMTEnMzYuMCJF!5e0!3m2!1sen!2ssa!4v1633024000000!5m2!1sen!2ssa"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3842407.205980734!2d43.31868612433816!3d19.867404322246575!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x15c3cfee75b226dd%3A0xa91278aa4a3265c4!2z2YLZhdmFINin2YTZhdiz2KfYrdipINin2YTYqtis2KfYsdmK2Kk!5e0!3m2!1sar!2ssa!4v1738527847601!5m2!1sar!2ssa"
             className="w-full h-96 border-0"
             allowFullScreen
             loading="lazy"
--- components/Partners.js
+++ components/Partners.js
@@ -7,7 +7,7 @@ const Partners = () => {
   const t = useTranslations("Partners");
 
   return (
-    <section className="py-2 bg-gray-300">
+    <section>
       <div className="container mx-auto px-4">
         <div className="flex justify-center">
           <h1 className="p-10 almarai-bold text-5xl text-black text-center">
